c8e8d3fc01bc862a2dbad6cf15a2a4fb46f71a53
慶田城 聖人 <blank491@gmail.com>
2018-11-08 13:00:14 +0900

    サーバーアップ用に修正

---
+++ htdocs/precache-manifest.640ede820b8690ac45d9500d0a5677a5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "30cf2abb3883bc3b54f0",
+    "url": "/js/chunk-vendors.0bf12be2.js"
+  },
+  {
+    "revision": "3d2f4ada5e7c9ac1a55d",
+    "url": "/js/app.a3a93c87.js"
+  },
+  {
+    "revision": "f9df968259376dca9492",
+    "url": "/js/about.555411c9.js"
+  },
+  {
+    "revision": "3645e9ff3fd752bede61dd8bee7fe2c1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "3d2f4ada5e7c9ac1a55d",
+    "url": "/css/app.c0b6ca68.css"
+  }
+];
